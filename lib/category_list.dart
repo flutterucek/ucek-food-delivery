567b8cff39e0d47b1f7ebf9feda15a45bd7e818f
Vishal DS <321vishalds@gmail.com>
2020-08-19 16:51:42 +0530

    Push amidst development. Not completed code.

--- lib/category_list.dart
+++ lib/category_list.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 class CategoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> categoryList = [
+      'Indian Food',
+      'Chineese Food',
+      'Italian Food',
+      'Kerala Food'
+    ];
     return Container(
       height: 120,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
-//          itemCount: categoryList.length,
+          itemCount: categoryList.length,
           itemBuilder: (context, index) {
             return Container(
               margin: EdgeInsets.only(right: 12),
-//                      height: 80,
+                      height: 80,
               width: 60,
               color: Colors.white,
-//              child: Text(categoryList[index]),
+              child: Text(categoryList[index]),
             );
           }),
     );
--- lib/ucek_appbar.dart
+++ lib/ucek_appbar.dart
@@ -1,13 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UcekAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-      height: 60,
-      width: MediaQuery.of(context).size.width,
-      color: Colors.white,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top+20),
+      child:Row(
+        children: <Widget> [
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child:ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: InkWell(
+                onTap: (){print("List button was clicked");},
+                child: SizedBox(
+                  height: 60,
+                  width: 60,
+                  child:DecoratedBox(
+                    decoration: BoxDecoration(color: Color(0xff404040)),
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width-170),
+            child:ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: InkWell(
+                onTap: (){print("Search button was clicked");},
+                child: SizedBox(
+                  height: 60,
+                  width: 60,
+                  child:DecoratedBox(
+                    decoration: BoxDecoration(color: Color(0xff404040)),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
